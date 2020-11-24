e92531a7637a9c14cab615199d9de9cb98980ac1
Benjamin Hooper <me@benhooper.org>
2020-11-23 23:46:21 -0600

    Initial Settings Launcher Plugin

---
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Settings/Main.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wox.Plugin;
+
+namespace Microsoft.Plugin.Settings
+{
+    public class Main : IPlugin, IPluginI18n
+    {
+        public void Init(PluginInitContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Result> Query(Query query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTranslatedPluginDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTranslatedPluginTitle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
